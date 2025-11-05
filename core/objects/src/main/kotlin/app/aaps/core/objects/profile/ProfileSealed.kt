b29743bc3930d125766a7b52dd33fb494465ea50
Philoul <legoupil.jp@free.fr>
2025-11-05 22:34:03 +0100

    Fix Autotune tests

--- core/objects/src/main/kotlin/app/aaps/core/objects/profile/ProfileSealed.kt
+++ core/objects/src/main/kotlin/app/aaps/core/objects/profile/ProfileSealed.kt
@@ -359,7 +359,6 @@ sealed class ProfileSealed(
     override fun toPureNsJson(dateUtil: DateUtil): JSONObject {
         val o = JSONObject()
         o.put("units", units.asText)
-        o.put("dia", dia)
         o.put("timezone", dateUtil.timeZoneByOffset(utcOffset).id ?: "UTC")
         // SENS
         val sens = JSONArray()
